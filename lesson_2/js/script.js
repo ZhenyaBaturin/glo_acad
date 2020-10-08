38601d8d0db61af2cdb68f6632a31cfdd641a47a
Zhenya Baturin <zhenyabaturin21.91@gmail.com>
2020-10-08 12:50:46 +0500

    lesson 1

---
+++ lesson_2/js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+// lesson 2, 08.02.2020
+const money = 'Привет мир!';
+const income = 'Какое-то сообщение';
+let addExpreses;
+let deposit;
+let mission;
+let period;
+
+alert(money);
+console.log(income);
