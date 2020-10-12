6d595ccf33360540b211f2d73f9a3fe62ffad24a
Zhenya Baturin <zhenyabaturin21.91@gmail.com>
2020-10-12 14:04:35 +0500

    lesson 3 12/10/2020

--- dif_lesson_2/main.js
+++ dif_lesson_2/main.js
@@ -1,10 +1,60 @@
 'use strict';
+// lesson 2
+// const num = 266219;
+// let count = 1;
+// for (let i = 0; i < num.toString().length; i++) {
+//     count *= +num.toString()[i];
+// }
+// console.log(count);
+// count = count ** 3;
+// alert(count.toString().slice(0, 2));
 
-const num = 266219;
-let count = 1;
-for (let i = 0; i < num.toString().length; i++) {
-    count *= +num.toString()[i];
+// lesson3
+const lang = prompt('Введите язык', 'ru-eng');
+const ru = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+const eng = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const namePerson = prompt("Пользователь, введите свое имя на русском языке");
+//3.1.a
+ if (lang === "ru") {
+    console.log(ru[new Date().getDay()]);
+} else if (lang === "eng") {
+    console.log(eng[new Date().getDay()]);
+} else {
+    console.log('Что-то пошло не так');
 }
-console.log(count);
-count = count ** 3;
-alert(count.toString().slice(0, 2));
+
+// 3.1.b
+switch (lang) {
+    case 'ru':
+        console.log(ru[new Date().getDay()]);
+        break;
+    case 'eng':
+        console.log(eng[new Date().getDay()]);
+        break;
+    default:
+        console.log('Что-то пошло не так');
+}
+
+// 3.1.c
+const n = [
+    ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
+    ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+];
+let count;
+if (lang === "ru") {
+    count = 0;
+} else if (lang === "eng") {
+    count = 1;
+} else {
+    console.log("Что-то пошло не так");
+}
+    console.log(n[count][new Date().getDay()]);
+
+// 3.2
+if (namePerson.toLowerCase() === 'максим') {
+    console.log("Преподаватель");
+} else if (namePerson.toLowerCase() === 'артем') {
+    console.log("Директор");
+} else {
+    console.log("Студент");
+}
--- lesson_1/js/script.js
+++ lesson_1/js/script.js
@@ -1,15 +1,22 @@
 'use strict';
 // lesson 1, 2, 08.02.2020
-const money = 70000;
-const income = '20000';
-const addExpreses = 'недвижимость, дипломы, курсовые проекты';
-const deposit = true;
+const money = +prompt("Ваш месячный доход?", "70000");
+const income = 'Фриланс';
+const addExpreses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
+const deposit = confirm("Есть ли у вас депозит в банке?");
 const mission = 500000; 
 const period = 12;
+
+const expenses1 = prompt ("Введите обязательную статью расходов?");
+const amount1 = +prompt ("Во сколько это обойдется?");
+const expenses2 = prompt ("Введите обязательную статью расходов?");
+const amount2 = +prompt ("Во сколько это обойдется?");
+const budgetMonth = money - amount1 - amount2;
 
 // alert(money);
 // console.log(income);
 
+// lesson 2
 console.log(typeof(money));
 console.log(typeof(income));
 console.log(typeof(deposit));
@@ -17,6 +24,21 @@ console.log(addExpreses.length);
 console.log(`Период равен ${period} месяцев`);
 console.log(`Цель заработать ${mission} рублей`);
 console.log(addExpreses.toLowerCase().split(', '));
+const budgetDay = Math.round(budgetMonth / 30);
 
-const budgetDay = money / 30;
-console.log(Math.round(budgetDay));
+
+// lesson 3
+console.log(`Бютжет на месяц ${budgetMonth} рублей`);
+console.log(`Сколько месяцев требуется для достижении цели: ${Math.ceil(mission/budgetMonth)} месяцев`);
+console.log(`Бютжет на день: ${budgetDay}`);
+if (budgetDay > 1200) {
+    console.log("У вас высокий уровень дохода");
+} else if (600 >= budgetDay <= 1200) {
+    console.log("У вас средний уровень дохода");
+} else if (budgetDay < 600) {
+    console.log("К сожалению у вас уровень дохода ниже среднего");
+} else {
+    console.log("Что-то пошло не так");
+}
+
+
