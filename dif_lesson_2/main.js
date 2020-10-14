66f01b30c1f8d53f46ef33f17c6ee64442021305
Zhenya Baturin <zhenyabaturin21.91@gmail.com>
2020-10-14 09:48:23 +0500

    lesson4 14/10/2020

--- dif_lesson_2/main.js
+++ dif_lesson_2/main.js
@@ -42,10 +42,24 @@ const n = [
 ];
 let count; 
  (lang === "ru") ? count = 0:
- (lang === "eng") ? count = 1 : console.log("Что-то пошло не так");
+ (lang === "eng") ? count = 1 : console.log("Введите верные данные");
  
     console.log(n[count][new Date().getDay()]);
 
 // 3.2
 (namePerson.toLowerCase() === 'максим') ? console.log("Преподаватель"):
 (namePerson.toLowerCase() === 'артем') ? console.log("Директор") : console.log("Студент");
+
+// lesson 4
+// 4.1
+const getValue = (a) => {
+    if (typeof a === 'string') {
+        a.trim();
+        if (a.trim().length > 30) {
+            alert(`${a.trim().substring(0, 30)}...`); 
+        }
+    } else {
+        alert( "Вы ввелли не строку");
+    }
+};
+getValue('Тут введен какой-то текст с большим количесвом строк');
