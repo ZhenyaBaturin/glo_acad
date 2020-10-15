0fb51e9139cbf3a1aadc114805ff02485855995c
Zhenya Baturin <zhenyabaturin21.91@gmail.com>
2020-10-15 19:45:20 +0500

    lesson_6 15/10/2020

--- lesson_6/lesson6.js
+++ lesson_6/lesson6.js
@@ -1,30 +1,46 @@
 'use strict';
-let num = Math.floor(Math.random() * 100 + 1);
 
-function getResult (num) {
+
+function game () {
+    let num = Math.floor(Math.random() * 100 + 1);
+    let x = 10;
     
-    let number = +prompt("Загадайте число от 1 до 100");
-        function getStatus () {
-
-            if (number < num  && number > 0) {
-                alert("Загаданное число меньше");
-                console.log(num);
-                getResult(num);
-            } else if( number > num) {
-                alert("Загаданное число больше");
-                getResult(num);
-            } else if (number === null || number === 0 ) {
-                alert("Игра закончена");
-            } else if (number == num) {
-                alert(`Вы выйграли !`);
-            }else if (typeof(number) !== 'number' || isNaN(number) || number === " " ) {
-                alert("Введите число");
-                getResult(num);
-            }
-        }  
-        getStatus () ;
-    }
-getResult(num);
-
+    function getResult (num) {
+        let number = +prompt("Загадайте число от 1 до 100");
+        if (x === 1){
+            let loser = confirm(`Попытки закончились, хотите сыграть еще?`);
+            if(loser) {
+                game(); 
+            } 
+              return alert("Игра закончена"); 
+        }
+          function getStatus () {
+                
+                if (number < num  && number > 0) {
+                    x--;
+                    alert(`Загаданное число меньше, осталось ${x} попытка(-ок) `);
+                    getResult(num);
+                } else if( number > num) {
+                    x--;
+                    alert(`Загаданное число больше, осталось ${x} попытка(-ок) `);
+                    getResult(num);
     
+                } else if ( number === 0 ) {
+                    alert("Игра закончена");
+                } else if (number == num) {
+                    let win = confirm(`Поздравляю, Вы угадали!!! Хотели бы сыграть еще?`);
+                    win ? game(): alert("Игра закончена");
+                        
+                }else if (typeof(number) !== 'number' || isNaN(number) || number === " " ) {
+                    alert("Введите число");
+                    getResult(num);
+                } 
+            }  
+           
+            getStatus () ;
+        }
+    
+    getResult(num);
+}
+game();
 
