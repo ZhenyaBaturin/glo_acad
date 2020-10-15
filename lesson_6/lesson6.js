becaed1124b405e20fde0ed23dbb3f499186585b
Zhenya Baturin <zhenyabaturin21.91@gmail.com>
2020-10-15 17:52:25 +0500

    lesson6 15/10/2020

--- lesson_6/lesson6.js
+++ lesson_6/lesson6.js
@@ -1,34 +1,29 @@
 'use strict';
-let num = Math.floor(Math.random() * 100);
+let num = Math.floor(Math.random() * 100 + 1);
 
 function getResult (num) {
     
-    console.log(num);
     let number = +prompt("Загадайте число от 1 до 100");
-    console.log(number);
-    let x = 0;
         function getStatus () {
-            if (number < num) {
+
+            if (number < num  && number > 0) {
                 alert("Загаданное число меньше");
                 console.log(num);
                 getResult(num);
-                return x += x;
             } else if( number > num) {
                 alert("Загаданное число больше");
                 getResult(num);
-                return x += x;
-            } else if (number === "null" || number === null || number === "") {
-                // alert("Игра закончена");
+            } else if (number === null || number === 0 ) {
+                alert("Игра закончена");
             } else if (number == num) {
-                alert(`Вы выйграли с ${x}(-ой) попытки `);
-            }else if (typeof(number) !== 'number' ) {
+                alert(`Вы выйграли !`);
+            }else if (typeof(number) !== 'number' || isNaN(number) || number === " " ) {
                 alert("Введите число");
-                // getResult(num);
+                getResult(num);
             }
         }  
         getStatus () ;
     }
-    
 getResult(num);
 
     
