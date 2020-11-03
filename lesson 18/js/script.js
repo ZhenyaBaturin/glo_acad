6abefdfdf299ec86dae2e5fc71b066df50eff5b2
Zhenya Baturin <zhenyabaturin21.91@gmail.com>
2020-11-03 15:12:08 +0500

    lesson 19 03.11.2020

--- lesson 18/js/script.js
+++ lesson 18/js/script.js
@@ -15,7 +15,7 @@ function countTimer(deadline) {
         minutes = Math.floor((timeRemaining / 60) % 60),
         hours = Math.floor(timeRemaining / 60 / 60);
         return {timeRemaining, hours, minutes, seconds};
-    };
+    }
     // подстовляет нули
     function oneZero(num) {
         if (num >= 0 && num < 10) {
@@ -38,4 +38,86 @@ function countTimer(deadline) {
     let idInterval = setInterval(updateClock, 1000);
 }
 countTimer('03 november 2020');
+//меню
+const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+        menu = document.querySelector('menu'),
+        closeBtn = document.querySelector('.close-btn'),
+        menuItems = menu.querySelectorAll('ul>li');
+    const handlerMenu = () => {
+        menu.classList.toggle('active-menu');
+    };
+    btnMenu.addEventListener('click', () => {
+        handlerMenu();
+    });
+    closeBtn.addEventListener('click', () => {
+        handlerMenu();
+    });
+    menuItems.forEach((item) => {
+        item.addEventListener('click', () => {
+            handlerMenu();
+        });
+    });
+};
+toggleMenu();
+
+//popup
+const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+        popupBtn = document.querySelectorAll('.popup-btn'),
+        popupClose = document.querySelector('.popup-close'),
+        popupContent = document.querySelector('.popup-content');
+    let count = -30;
+    popupBtn.forEach((item) => {
+        item.addEventListener('click', () => {
+            if(document.documentElement.clientWidth < 768) {
+                popup.style.display = 'block';
+            } else {
+                popup.style.display = 'block';
+                popupContent.style.top = '-30%';
+                let getAnimPopup = () => {
+                    count++;
+                    if(count < 11) {
+                        popupContent.style.top = `${count}%`;
+                    } else {
+                        clearInterval(idInterval);
+                        count = -30;
+                    }
+                };
+                let idInterval = setInterval(getAnimPopup, 10);
+                }
+
+        });
+    });
+    popupClose.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
+
+};
+togglePopUp();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
