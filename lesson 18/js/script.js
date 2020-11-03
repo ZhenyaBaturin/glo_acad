bdd7377fdc8202d17209b297a78c05395d461aa6
Zhenya Baturin <zhenyabaturin21.91@gmail.com>
2020-11-03 16:08:11 +0500

    lessob 19 03/11/2020

--- lesson 18/js/script.js
+++ lesson 18/js/script.js
@@ -95,6 +95,20 @@ const togglePopUp = () => {
 
 };
 togglePopUp();
+// стрелка скролл
+const toggleStr = () => {
+    const arrow = document.querySelector('main>a');
+    arrow.addEventListener('click', (e) => {
+        e.preventDefault();
+        const idBlock = arrow.getAttribute('href');
+        console.log(idBlock);
+        document.querySelector(idBlock).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+          });
+    });
+};
+toggleStr();
 
 
 
